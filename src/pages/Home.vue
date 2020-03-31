1b2c7e7d15af7b25a5d082f10f7944f3d2fef358
DevilTea <ch870814@gmail.com>
2020-03-31 14:16:06 +0800

    Remove Home page useless components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="home" class="main-container">
-    <div class="background-video">
-      <video id="video-player" class="video-container" autoplay="autoplay" loop="true" muted="true" playsInine playsinline>
-        <source class="video" src="https://sitcon.org/2020/video/bg-h264.mp4" type="video/mp4">
-      </video>
-    </div>
     <header class="header">
       <div class="inner-container">
         <h1 class="title font-black">SITCON 2020</h1>
@@ -36,11 +31,7 @@
       </div>
     </div>
     <div class="spotlight-container">
-      <SitconTatonMask :width="sitconTatonMaskWidth" :height="sitconTatonMaskHeight"/>
-      <div class="spotlight sharp-back-taton sitcon-back-taton" ref="sitconTatonMask">
-        <div class="sitcon placeholder">
-          <img src="@/../public/img/icons/SITCON-taton-cliped.svg" alt="">
-        </div>
+      <div class="spotlight sharp-back-taton">
         <h1 class="title has-prefix-icon">
           <img class="prefix-icon" src="@/assets/images/sharp-white.svg" />
           <span class="inner font-black">什麼是 SITCON</span>
@@ -49,46 +40,6 @@
         <p class="content">學生計算機年會（Students’ Information Technology Conference）自 2013 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。</p>
         <p class="content">二十一世紀已經過了五分之一，今年，我們期待帶著豐沛熱情的你，能和我們一起在 2020 年這麼一個「承先啟後」的年份，綜觀不同領域發展與事件脈絡，自主學習感興趣之專長與技術，彼此分享、相濡以沫。在 SITCON 2020 裡，願所有與會者皆能拓展視野，鍛鍊與發展自我，找到向前邁步的勇氣與目標。相信身為學生的我們，也將能重新定義自我、定義未來！</p>
         <p class="content">SITCON 2020 邀請身為學生的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 <a href="#call-for-define">#define student</a>。</p>
-      </div>
-      <div
-        id="call-for-define"
-        :class="{ 'on-send': onSend }"
-        class="spotlight sharp-back-taton"
-        ref="bulletScreen"
-      >
-        <BulletScreen
-          class="sketch-box"
-          :class="{ 'on-send': onSend }"
-          :msg="defineString"
-          :width="bulletScreenWidth"
-          :height="bulletScreenHeight"
-          :active="onSend"
-          :quality="defineString.replace(/[^\x00-\xff]/g, 'xx').length > 10 ? 25 : 50"
-        />
-        <h1 class="title has-prefix-icon">
-          <img class="prefix-icon" src="@/assets/images/sharp-white.svg" />
-          <span class="inner font-black">Call for #define</span>
-          <span class="inner font-black">#define student</span>
-        </h1>
-        <p class="content">今年 SITCON 以 #define student 作為 slogan，希望能夠重新定義學生的價值。同時我們也想知道有什麼大家想瞭解的議題或重新定義的事物，不論是你期望在 SITCON 見到的議題、認同的字彙、想要大家知道的技術，歡迎來和大家分享你的看法，一起 define anything。</p>
-        <div class="input-box">
-          <label for="define"><span class="font-black">#define</span></label>
-          <input
-            v-model="defineString"
-            type="text"
-            name="define"
-            placeholder="student"
-            class="font-black"
-            maxlength="30"
-            @keypress.enter="sendDefineForm"
-          >
-          <span
-            class="icon"
-            @click="sendDefineForm"
-          >
-            <font-awesome-icon :icon="['far', 'paper-plane']" />
-          </span>
-        </div>
       </div>
       <div class="spotlight sharp-back-taton">
         <h1 class="title has-prefix-icon">
@@ -200,95 +151,14 @@
 
 <script lang="ts">
 import { Component, Watch, Vue } from 'vue-property-decorator';
-import { Action, Getter } from 'vuex-class';
 import axios from 'axios';
-
-import SitconTatonMask from '@/components/SitconTatonMask.vue';
-import BulletScreen from '@/components/CFP/BulletScreen.vue';
-
-import { ThemeType } from '../store/types/app';
 
 import head from '@/util/head';
 
-@Component({
-  components: {
-    BulletScreen,
-    SitconTatonMask
-  }
-})
+@Component
 export default class Home extends Vue {
-  @Action('toggleTheme', { namespace: 'app' }) private toggleTheme!: (theme: ThemeType) => void;
-
-  private sitconTatonMaskHeight: number = 0;
-  private sitconTatonMaskWidth: number = 0;
-  private defineString: string = '';
-  private onSend: boolean = false;
-  private bulletScreenWidth: number = 0;
-  private bulletScreenHeight: number = 0;
-
-  @Watch('$route')
-  public onRouteChange () {
-    this.continuePlayVideoWhenFocusBack();
-  }
-
   public mounted () {
     head.reset();
-    this.measureMaskSize();
-    this.measureBulletScreenSize();
-    this.continuePlayVideoWhenFocusBack();
-    window.addEventListener('resize', this.measureMaskSize);
-    window.addEventListener('resize', this.measureBulletScreenSize);
-  }
-
-  public destroyed () {
-    window.removeEventListener('resize', this.measureMaskSize);
-    window.removeEventListener('resize', this.measureBulletScreenSize);
-  }
-
-  private measureMaskSize (): void {
-    this.sitconTatonMaskHeight = (this.$refs.sitconTatonMask as Element).clientHeight;
-    this.sitconTatonMaskWidth = (this.$refs.sitconTatonMask as Element).clientWidth;
-  }
-
-  private continuePlayVideoWhenFocusBack (): void {
-    const videoPlayerDOM = document.getElementById('video-player') as HTMLMediaElement;
-    if (videoPlayerDOM && videoPlayerDOM.paused) {
-      videoPlayerDOM.play();
-    }
-  }
-
-  private toggleSendAnimate (): void {
-    this.onSend = true;
-    setTimeout(() => {
-      this.defineString = '';
-      this.onSend = false;
-    }, 3000);
-  }
-
-  private measureBulletScreenSize (): void {
-    const bulletScreen: any = this.$refs.bulletScreen;
-
-    this.bulletScreenWidth = bulletScreen.clientWidth;
-    this.bulletScreenHeight = bulletScreen.clientHeight;
-  }
-
-  private async sendDefineForm (): Promise<void> {
-    if (this.defineString) {
-      try {
-        // Detect Egg
-        if (this.defineString.toLowerCase().trim().replace(/ /g, '') === 'genderequality') {
-          this.toggleTheme(ThemeType.RAINBOW_DARK);
-        }
-
-        const defineString = this.defineString;
-        this.toggleSendAnimate();
-        await axios.post(`https://docs.google.com/forms/d/e/1FAIpQLSf60kH6sRpI_7ctpKi8ptcI7cG2OpvGmW3SjWUGcyiDEekQ4w/formResponse?entry.2121946644=${defineString}`);
-      } catch (error) {
-        // tslint:disable:no-console
-        console.error('Google Form POST', error);
-        // tslint:enable
-      }
-    }
   }
 }
 </script>
