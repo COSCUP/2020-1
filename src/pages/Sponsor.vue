0459ead336a82fcd8a4a3c7de1481913e6dfe18b
DevilTea <ch870814@gmail.com>
2020-04-18 20:17:28 +0800

    Re-arrange scss files structure

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -1,60 +1,71 @@
 <template>
-  <div id="team" class="main-container">
-    <div id="sponsor">
-      <div class="sponsor-container">
-        <div class="card-container individual-sponsor">
-          <div class="card">
-            <h2 class="font-black subtitle">我們歡迎個人贊助</h2>
-            <p
-              class="paragraph"
-            >SITCON 學生計算機年會為學生自發組成籌辦的研討會，秉持著以學生為主軸的核心價值，提供一個經驗交流與資訊技術實務分享的平台，進而達到「學以致用、教學相長」的目標。</p>
-            <p class="paragraph">
-              維繫這樣的交流平台，需要多人的努力及貢獻，亦十分需要在資金上的支持。
-              <br />希望能藉由您的一份力量，來促成活動的舉行，讓更多學生能在舞台上展現自我成長。
-              <br />歡迎您參與贊助，支持 SITCON、讓我們將每年的活動辦得盡善盡美！
-            </p>
-            <p class="paragraph">
-              <a
-                href="https://bit.ly/donatesitcon2020"
-                target="_blank"
-                rel="noopener"
-                class="font-bold"
-              >個人贊助辦法</a>
-            </p>
-            <p class="paragraph">
-              企業贊助亦歡迎來信
-              <a class="font-bold" href="mailto:contact@sitcon.org">contact@sitcon.org</a>
-            </p>
-          </div>
-        </div>
-        <div
-          v-for="level in Object.keys(sponsorList)"
-          :key="`sponsor-level-${level}`"
-          class="sponsor-wrapper"
-        >
-          <h2 class="level font-bold">{{ sponsorLevelText[level] }}</h2>
-          <div :class="`card-container ${level === 'holder' ? 'host-container' : 'org-container'}`">
-            <div
-              v-for="sponsor in sponsorList[level]"
-              :id="sponsor.slug"
-              :key="sponsor.slug"
-              :class="[
-              `card ${level === 'holder' ? 'host' : 'org'}`,
-              sponsorList[level].length === 1 ? 'horizen': ''
-            ]"
+  <div id="sponsor" class="main-container">
+    <div class="sponsor-container">
+      <div class="card-container individual-sponsor">
+        <div class="card">
+          <h2 class="font-black subtitle">我們歡迎個人贊助</h2>
+          <p class="paragraph">
+            SITCON
+            學生計算機年會為學生自發組成籌辦的研討會，秉持著以學生為主軸的核心價值，提供一個經驗交流與資訊技術實務分享的平台，進而達到「學以致用、教學相長」的目標。
+          </p>
+          <p class="paragraph">
+            維繫這樣的交流平台，需要多人的努力及貢獻，亦十分需要在資金上的支持。
+            <br />希望能藉由您的一份力量，來促成活動的舉行，讓更多學生能在舞台上展現自我成長。
+            <br />歡迎您參與贊助，支持 SITCON、讓我們將每年的活動辦得盡善盡美！
+          </p>
+          <p class="paragraph">
+            <a
+              href="https://bit.ly/donatesitcon2020"
+              target="_blank"
+              rel="noopener"
+              class="font-bold"
+              >個人贊助辦法</a
             >
-              <div class="img-container">
-                <a :href="`${sponsor.url}`" target="_blank" rel="noopener">
-                  <img
-                    :alt="sponsor.name"
-                    :src="`https://sitcon.org/2020/img/sponsors/${sponsor.image}`"
-                  />
-                </a>
-              </div>
-              <div class="text-container">
-                <h3>{{ sponsor.name }}</h3>
-                <p v-if="sponsor.description" v-html="markdownParser(sponsor.description)"></p>
-              </div>
+          </p>
+          <p class="paragraph">
+            企業贊助亦歡迎來信
+            <a class="font-bold" href="mailto:contact@sitcon.org"
+              >contact@sitcon.org</a
+            >
+          </p>
+        </div>
+      </div>
+      <div
+        v-for="level in Object.keys(sponsorList)"
+        :key="`sponsor-level-${level}`"
+        class="sponsor-wrapper"
+      >
+        <h2 class="level font-bold">{{ sponsorLevelText[level] }}</h2>
+        <div
+          :class="
+            `card-container ${
+              level === 'holder' ? 'host-container' : 'org-container'
+            }`
+          "
+        >
+          <div
+            v-for="sponsor in sponsorList[level]"
+            :id="sponsor.slug"
+            :key="sponsor.slug"
+            :class="[
+              `card ${level === 'holder' ? 'host' : 'org'}`,
+              sponsorList[level].length === 1 ? 'horizen' : ''
+            ]"
+          >
+            <div class="img-container">
+              <a :href="`${sponsor.url}`" target="_blank" rel="noopener">
+                <img
+                  :alt="sponsor.name"
+                  :src="`https://sitcon.org/2020/img/sponsors/${sponsor.image}`"
+                />
+              </a>
+            </div>
+            <div class="text-container">
+              <h3>{{ sponsor.name }}</h3>
+              <p
+                v-if="sponsor.description"
+                v-html="markdownParser(sponsor.description)"
+              ></p>
             </div>
           </div>
         </div>
--- src/pages/Staff.vue
+++ src/pages/Staff.vue
@@ -1,35 +1,41 @@
 <template>
-  <div id="team" class="main-container">
-    <div id="staff" class="staff-container">
-      <div class="staff-spotlight full-width-banner">
-        <p
-          class="paragraph"
-        >SITCON 年會每年皆是許多志工奉獻與時間精神所舉辦；若你對參與 SITCON 年會的籌備有興趣，歡迎填寫表單，我們將在明年活動開始籌備時通知您！</p>
-        <p class="paragraph">
-          <a
-            href="https://forms.gle/1J9fxQP2ux55PJXC9"
-            class="tone-trans form-link font-bold"
-            target="_blank"
-            rel="noopener"
-          >表單連結</a>
-        </p>
-      </div>
-      <div
-        v-for="group in staffs"
-        :key="`staff-group-${group.name}`"
-        class="staff-spotlight staff-group"
-      >
-        <h2 class="staff-group-name font-bold">{{ group.name }}</h2>
-        <p class="paragraph staff-group-intro">{{ group.description }}</p>
-        <div class="staff-wrapper">
-          <div v-for="member in group.members" :key="`${group.name}-${member.name}`" class="staff">
-            <img
-              class="staff-avatar"
-              :src="`https://www.gravatar.com/avatar/${member.emailHash}?s=320&d=https://sitcon.org/2020/img/sitcon-logo.png&r=g`"
-              :alt="`${member.name}'s Avatar`"
-            />
-            <p>{{ member.name }}</p>
-          </div>
+  <div id="staff" class="main-container staff-container">
+    <div class="staff-spotlight full-width-banner">
+      <p class="paragraph">
+        SITCON 年會每年皆是許多志工奉獻與時間精神所舉辦；若你對參與 SITCON
+        年會的籌備有興趣，歡迎填寫表單，我們將在明年活動開始籌備時通知您！
+      </p>
+      <p class="paragraph">
+        <a
+          href="https://forms.gle/1J9fxQP2ux55PJXC9"
+          class="tone-trans form-link font-bold"
+          target="_blank"
+          rel="noopener"
+          >表單連結</a
+        >
+      </p>
+    </div>
+    <div
+      v-for="group in staffs"
+      :key="`staff-group-${group.name}`"
+      class="staff-spotlight staff-group"
+    >
+      <h2 class="staff-group-name font-bold">{{ group.name }}</h2>
+      <p class="paragraph staff-group-intro">{{ group.description }}</p>
+      <div class="staff-wrapper">
+        <div
+          v-for="member in group.members"
+          :key="`${group.name}-${member.name}`"
+          class="staff"
+        >
+          <img
+            class="staff-avatar"
+            :src="
+              `https://www.gravatar.com/avatar/${member.emailHash}?s=320&d=https://sitcon.org/2020/img/sitcon-logo.png&r=g`
+            "
+            :alt="`${member.name}'s Avatar`"
+          />
+          <p>{{ member.name }}</p>
         </div>
       </div>
     </div>
