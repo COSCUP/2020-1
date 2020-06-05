8e7186d39dd017759ee2c7011da34dcb4ece3a8f
DevilTea <ch870814@gmail.com>
2020-06-05 21:35:58 +0800

    Add staff.json generate script

---
+++ src/util/staff/index.js
@@ -0,0 +1,7 @@
+const { default: axios } = require('axios')
+
+axios.get('https://volunteer.coscup.org/api/members?pid=2020')
+  .then(({ data: { data } }) => {
+    console.log(JSON.stringify(data))
+  })
+  .catch(() => process.exit(1))
