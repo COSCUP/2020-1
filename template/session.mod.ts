722c9bef9ceaa0cd524ff2f9681cf055d60fb335
DevilTea <ch870814@gmail.com>
2020-06-12 18:43:29 +0800

    Update session template

--- template/session.mod.ts
+++ template/session.mod.ts
@@ -28,7 +28,8 @@ const content = (data: any, language: 'zh' | 'en'): string => data ? `
 }
 </script>
 <div>
-  <h1 class="title font-black">${data[language].title}</h1>
+  <span style="color: #3b9838; font-size: 1.25rem;">${getDay(data.start)}</span>
+  <h1 class="title" style="margin-top: 8px; margin-bottom: 12px;"><span class="font-black" style="display: inline-block; margin-right: 1rem; line-height: 1.25;">${data[language].title}</span><span style="margin: 8px 0; display: inline-block; vertical-align: top; font-size: 0.8rem; padding: 8px 1rem; color: #fff; background-color: #3b9838; border-radius: 25px;">${data.tags[0][language].name}</span></h1>
   <h2 class="font-black" style="color: #3b9838; margin-bottom: 0;">${data.type[language].name}</h2>
   <div style="margin-bottom: 2rem;">
     <span class="font-black">${data.room[language].name.split(' / ')[0]}</span>
@@ -57,6 +58,20 @@ const contentParser = (content: string): string => (
 const formatTimeString = (date: Date, joinChar = '') => {
   return [date.getHours(), date.getMinutes()]
     .map((digit) => digit.toString().padStart(2, '0')).join(joinChar)
+}
+
+const formatDateString = (date: Date, joinChar = '') => {
+  return [date.getMonth() + 1, date.getDate()]
+    .map((digit) => digit.toString().padStart(2, '0')).join(joinChar)
+}
+
+const getDay = (date: Date) => {
+  const dateStr = formatDateString(date) as '0801' | '0802'
+  const day = {
+    '0801': 'Day 1',
+    '0802': 'Day 2'
+  }
+  return `${day[dateStr]} (${dateStr})`
 }
 
 export default content
